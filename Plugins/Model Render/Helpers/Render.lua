0d1dcdbf4c80764818eca56a8c6654424085f69f
DesyncDeveloper <111148565+DesyncDeveloper@users.noreply.github.com>
2025-01-17 22:06:14 +0000

    Update Render.lua

--- Plugins/Model Render/Helpers/Render.lua
+++ Plugins/Model Render/Helpers/Render.lua
@@ -94,4 +94,6 @@ function Render.RenderModelInViewport(data)
 	)
 end
 
+-- require(game.ReplicatedStorage.Render).renderModelInViewport("Huge Bunny", {Distance = 0, Scale = false, RotationX = 102, RotationY = 0, RotationZ = 0})
+
 return Render
